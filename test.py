4fb78a98bc1f4fef4633193b48a2b7382e583fa0
2005Payne <88232976+2005Payne@users.noreply.github.com>
2022-04-22 12:21:44 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+from ntpath import join
+
+
+a=list(input())
+key=int(input())
+for i in range(len(a)):
+    if(ord(a[i])<91 and ord(a[i])>=65):
+        a[i]=chr(((ord(a[i])-65+key)%26)+65)
+    elif(ord(a[i])>=97 and ord(a[i])<=122):
+        a[i]=chr(((ord(a[i])-97+key)%26)+97)
+for i in a:
+    print(i,end="")
